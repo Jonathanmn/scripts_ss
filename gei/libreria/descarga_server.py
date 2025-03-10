91805ca9ed0fdbf928c3ece6b59626d38de1cbfc
jmn <kidrespaldos@gmail.com>
2025-03-10 00:46:12 -0600

    update to upload server

--- gei/libreria/descarga_server.py
+++ gei/libreria/descarga_server.py
@@ -5,8 +5,8 @@ from stat import S_ISDIR
 hostname = "132.248.8.32"
 username = "gei"
 password = "ru04g31"
-remote_path = "/home/gei/scripts_j/l0"
-local_path = "/home/jonathan_mn/server_gei"
+#remote_path = "/home/gei/scripts_j/l0"
+#local_path = "/home/jonathan_mn/server_gei"
 
 
 
@@ -64,23 +64,65 @@ def upload_file_to_server(hostname, username, password, local_file_path, remote_
         print(f"No se pudo subir el archivo: {e}")
 
 # Argumentos para la descarga
+def upload_file_or_folder_to_server(hostname, username, password, local_path, remote_path):
+    """
+    Sube un archivo o carpeta desde una ruta local a una ruta remota en el servidor
+    """
+    try:
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh_client.connect(hostname, username=username, password=password)
+        sftp_client = ssh_client.open_sftp()
+
+        def _upload_recursive(local_dir, remote_dir):
+            os.makedirs(local_dir, exist_ok=True)
+            try:
+                sftp_client.mkdir(remote_dir)
+            except IOError:
+                pass  # Directorio ya existe
+
+            for item in os.listdir(local_dir):
+                local_path = os.path.join(local_dir, item)
+                remote_path = os.path.join(remote_dir, item)
+                if os.path.isdir(local_path):
+                    _upload_recursive(local_path, remote_path)
+                else:
+                    sftp_client.put(local_path, remote_path)
+
+        if os.path.isdir(local_path):
+            _upload_recursive(local_path, remote_path)
+        else:
+            sftp_client.put(local_path, remote_path)
+
+        sftp_client.close()
+        ssh_client.close()
+
+        print(f"'{local_path}' se subió a '{remote_path}'")
+
+    except Exception as e:
+        print(f"No se pudo subir '{local_path}': {e}")
 
 
 # Descargar carpeta
-download_roua_process(hostname, username, password, remote_path, local_path)
+#download_roua_process(hostname, username, password, remote_path, local_path)
 
 # Argumentos para la subida
 
 
-local_file_path = "/home/jmn/ss/scripts/git/scripts_ss/gei/libreria/gei_raw_to_l0.py"
-remote_file_path = "/home/gei/scripts_j/gei_raw_to_l0.py"
+#local_file_path = "/home/jmn/ss/scripts/git/scripts_ss/gei/libreria/gei_raw_to_l0.py"
+#remote_file_path = "/home/gei/scripts_j/gei_raw_to_l0.py"
 
 # Subir archivo
 #(hostname, username, password, local_file_path, remote_file_path)
 
 
 
-local_file_path = "/home/jmn/ss/scripts/git/scripts_ss/gei/libreria/picarro_l0_server.py"
-remote_file_path = "/home/gei/scripts_j/picarro_l0_server.py"
+#local_file_path = "/home/jmn/ss/scripts/git/scripts_ss/gei/libreria/picarro_l0_server.py"
+#remote_file_path = "/home/gei/scripts_j/picarro_l0_server.py"
 
 #upload_file_to_server(hostname, username, password, local_file_path, remote_file_path)
+local_folder_path='/home/jmn/l0-1'
+remote_folder_path = "/home/gei/scripts_j/L0"
+
+
+upload_file_or_folder_to_server(hostname, username, password, local_folder_path, remote_folder_path)
