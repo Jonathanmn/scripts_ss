bbc6ceb0f1dbd6b46f8e2c1447030224618bc7bc
jmn_lab <jonathanmalagon@encit.unam.mx>
2025-04-03 14:00:14 -0600

    cambios

--- plots_dinamicos/plot_scatter_gei_pm.py
+++ plots_dinamicos/plot_scatter_gei_pm.py
@@ -41,38 +41,6 @@ merged_data = merge_df(
 
 
 
-def analisis_variables(df, column1, column2, timestamp_column='Timestamp'):
-    """
-hace un scatter plot entre variables 
-    """
-    fig, ax1 = plt.subplots()
-    marker_size=1
-    # Plot column1 on the primary y-axis
-    ax1.scatter(df[timestamp_column], df[column1], color='blue', label=column1, alpha=0.7,s=marker_size)
-    ax1.plot(df[timestamp_column], df[column1], color='blue', alpha=0.2, linewidth=.5)
-    ax1.set_xlabel(timestamp_column)
-    ax1.set_ylabel(column1, color='blue')
-    ax1.tick_params(axis='y', colors='blue')
-    ax1.tick_params(axis='x', rotation=45)
-
-    # Create a secondary y-axis (twinx)
-    ax2 = ax1.twinx()
-    ax2.scatter(df[timestamp_column], df[column2], color='red', label=column2, alpha=0.7,s=marker_size)
-    ax2.plot(df[timestamp_column], df[column2], color='red', alpha=0.3, linewidth=.5)
-    ax2.set_ylabel(column2, color='red')
-    ax2.tick_params(axis='y', colors='red')
-
-    # Add a legend
-    fig.legend(loc='upper right', bbox_to_anchor=(0.85, 0.85))
-
-    # Show the plot
-    plt.title(f'Scatter Plot: {column1} and {column2} over {timestamp_column}')
-    plt.tight_layout()
-    plt.show()
-
-
-#analisis_variables(merged_data, 'CO2_Avg', 'PM2.5 Conc', timestamp_column='Timestamp')
-
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -105,48 +73,6 @@ def correlacion(df, column1, column2):
 
 
 
-
-
-
-
-
-
-def correlacion(df, column1, column2):
-    """
-    Realiza un scatter plot para mostrar la correlación entre dos columnas
-    y añade el valor de la correlación de Pearson y R² en el gráfico.
-    """
-    # Eliminar filas con valores NaN en cualquiera de las dos columnas
-    filtered_df = df[[column1, column2]].dropna()
-
-    # Calcular la correlación de Pearson
-    correlation, _ = pearsonr(filtered_df[column1], filtered_df[column2])
-
-    # Calcular el valor de R²
-    r_squared = correlation ** 2
-
-    # Crear el scatter plot
-    plt.figure(figsize=(8, 6))
-    plt.scatter(x=filtered_df[column1], y=filtered_df[column2], alpha=0.7, color='red', s=1)
-
-    # Añadir el valor de la correlación y R² en el gráfico
-    plt.title(
-        f'Scatter Plot: {column1} vs {column2}\n'
-        f'Pearson Correlation: {correlation:.2f}\n'
-        f'R²: {r_squared:.2f}',
-        fontsize=14
-    )
-    plt.xlabel(column1, fontsize=12)
-    plt.ylabel(column2, fontsize=12)
-    plt.grid(True, linestyle='--', alpha=0.6)
-
-    # Mostrar el gráfico
-    plt.tight_layout()
-    plt.show()
-
-
-
-import numpy as np
 
 def correlacion1(df, column1, column2):
     """
