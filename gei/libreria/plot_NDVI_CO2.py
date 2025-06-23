07ce2deecbc634cbea5477a46e805efc926a785a
jonathan_pc_ace <jonathanmalagon@encit.unam.mx>
2025-06-23 11:39:30 -0600

    ndcvi

--- gei/libreria/plot_NDVI_CO2.py
+++ gei/libreria/plot_NDVI_CO2.py
@@ -39,7 +39,9 @@ def resample24_month(df):
     
     df_monthly_avg_24h = df_resampled.groupby(['Mes', 'Hora']).mean().reset_index()
     df_month=df_monthly_avg_24h.groupby('Mes').mean().reset_index()
-    df_month['Mes'] = df_month['Mes'].astype(int)
+    df_month['Mes'] = df_month['Mes']
+    df_month['Mes'] = pd.to_datetime(df_month['Mes'], format='%m').dt.strftime('%m')
+    df_month['CO2_Avg'] = df_month['CO2_Avg'].round(3)
     df_month= df_month[['Mes', 'CO2_Avg']]
     return df_month
 
@@ -58,7 +60,7 @@ gei_monthly['Time'] = gei_monthly['Time'].dt.strftime('%m')
 print(gei_monthly.head())
 
 
-
+'''
 
 
 ndvi_avg = pd.read_csv(ndvi_folder_path, sep=',', header=0,)
@@ -75,8 +77,8 @@ print(ndvi_avg)
 
 
 # Unimos los dataframes por mes
-ndvi_co2 = pd.merge(gei_monthly, ndvi_avg, left_on='Time', right_on='Mes', how='inner')
-ndvi_co2 = ndvi_co2[['Time', 'CO2_Avg', 'NDVI_Mean']]
+ndvi_co2 = pd.merge(geib, ndvi_avg, left_on='Mes', right_on='Mes', how='inner')
+ndvi_co2 = ndvi_co2[['Mes', 'CO2_Avg', 'NDVI_Mean']]
 print(ndvi_co2)
 
 
@@ -84,28 +86,31 @@ print(ndvi_co2)
 # ploteamos los datos
 plt.figure(figsize=(12, 6))
 
-# Create primary y-axis
+
+axx=ndvi_co2['Mes']
+
+
 ax1 = plt.gca()
 ax1.set_xlabel('Month')
 ax1.set_ylabel('CO2 (ppm)', color='blue')
-ax1.plot(ndvi_co2['Time'], ndvi_co2['CO2_Avg'], marker='o', color='blue', label='CO2 Avg')
+ax1.plot(axx, ndvi_co2['CO2_Avg'], marker='o', color='blue', label='CO2 Avg')
 ax1.tick_params(axis='y', labelcolor='blue')
+ax1.set_ylim(420, 450)  
 
-# Create secondary y-axis
+
 ax2 = ax1.twinx()
 ax2.set_ylabel('NDVI', color='green')
-ax2.plot(ndvi_co2['Time'], ndvi_co2['NDVI_Mean'], marker='x', color='green', label='NDVI Mean')
+ax2.plot(axx, ndvi_co2['NDVI_Mean'], marker='x', color='green', label='NDVI Mean')
 ax2.tick_params(axis='y', labelcolor='green')
+ax2.set_ylim(0,1)
 
-# Add combined legend
 lines1, labels1 = ax1.get_legend_handles_labels()
 lines2, labels2 = ax2.get_legend_handles_labels()
 ax1.legend(lines1 + lines2, labels1 + labels2, loc='best')
 
-plt.title('Monthly CO2 vs NDVI (2024)')
+plt.title('CO2 y NDVI (2024)')
 plt.grid(True, alpha=0.3)
 plt.tight_layout()
 plt.show()
 
 
-'''
