d278c53d605a2019abc50879002fd52e2e11cbfd
jonathan_pc_ace <jonathanmalagon@encit.unam.mx>
2025-08-25 15:11:56 -0600

    cambvios en pm

--- 3_t64_pm/read_t64pm.py
+++ 3_t64_pm/read_t64pm.py
@@ -9,23 +9,30 @@ from windrose import WindroseAxes
 
 folder_t64 = 'C:/git_mn/scripts_ss/_files/pm/L0/minuto'
 
-# Get all .txt files in the folder
+
 txt_files = glob.glob(os.path.join(folder_t64, '*.txt'))
 
-# Read all files and combine them
 dataframes = []
 for file in txt_files:
-    df = pd.read_csv(file, sep=',', header=0)
-    # Optional: add a column to identify the source file
-    
+    usecols = ['Date & Time (Local)', '  PM2.5 Conc', '  PM10 Conc']
+    df = pd.read_csv(file, sep=',', header=0, usecols=usecols)
     dataframes.append(df)
 
-# Combine all dataframes
-if dataframes:
-    combined_df = pd.concat(dataframes, ignore_index=True)
-    print(f"Read {len(txt_files)} files with {len(combined_df)} total rows")
-else:
-    print("No .txt files found in the folder")
+t64_=pd.concat(dataframes,ignore_index=True)
+t64_['Date & Time (Local)']=pd.to_datetime(t64_['Date & Time (Local)'])
+t64_=t64_.sort_values(by='Date & Time (Local)')
+t64_=t64_.reset_index(drop=True)
 
 
+print(t64_.columns)
 
+
+#rename columns
+t64_.rename(columns={
+    'Date & Time (Local)': 'datetime',
+    '  PM2.5 Conc': 'pm25',
+    '  PM10 Conc': 'pm10'
+}, inplace=True)
+
+
+print(t64_.columns)
